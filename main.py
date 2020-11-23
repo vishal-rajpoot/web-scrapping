d2837e765fc744750b93f3e5f3318558bf30d7d6
vishal rajput <vishalrajpoot540@gmail.com>
2020-11-23 19:27:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-import requests
-import bs4
- 
+# IMPORT MODULES
+import requests   # "REQUEST" MODULE DEFINES FUNCTIONS AND CLASSES WHICH HELP IN OPENING URL'S.
+import bs4        # HERE "BEAUTIFUL SOUP" MODULE DEFINES FUNCTIONS AND CLASSES WHICH HELP TO PARSE THE HTML TEXT IN A PROPER READABLE FORMAT.
 
 url = input("enter your url ")
-response = requests.get(url)
+response = requests.get(url) #HERE WHEN OUR REQUEST GETS APPROVED AND WE WILL GET RESPONSE FROM THE SERVER OF THE GIVEN URL 
 
 print(type(response))
-print(response.text)
-filename = "temp.html"
-bs = bs4.BeautifulSoup(response.text,"html.parser")
+# print(response.text)  # HERE WE PRINT THE RESPONSE TEXT FROM THE SERVER.
 
-formatted_text = bs.prettify()
+filename = "temp.html"   # HERE WE CREATED TEMPORARY HTML FILE TO SAVE THE REQUESTED CONTENT FROM THE URL.
+bs = bs4.BeautifulSoup(response.text,"html.parser") # HERE WE CREATED "bs4" OBJECT TO PARSE THE HTML TEXT IN A READABLE FORMAT 
+
+formatted_text = bs.prettify() # HERE prettify() METHOD WILL TURN A BEAUTIFUL SOUP PARSE TREE INTO A NICELY FORMATTED UNICODE STRING, WITH A SEPERATE LINE FOR EACH TAG AND EACH STRING.
 print(formatted_text)
 
-with open(filename, "w+") as f:
+with open(filename, "w+") as f:  # HERE "with" KEYWORD IS USED FOR AUTOMATICALLY OPEN THE FILE USING "open() FUNCTION AS A WRITEABLE MODE.
     f.write(formatted_text)
 
-list_imgs = bs.find_all('img')
+list_imgs = bs.find_all('img')  # HERE WE USED find_all() function TO FIND ALL THE "IMG" TAGS IN A FORMATTED HTML TEXT 
 print(list_imgs)
 
-no_of_imgs = len(list_imgs)
+no_of_imgs = len(list_imgs) # HERE WE PRINTED LENGTH OF THE IMG TAGS
 
-list_as = bs.find_all('a')
+list_as = bs.find_all('a') # HERE WE USED find_all() function TO FIND ALL THE "ANCHOR" TAGS IN A FORMATTED HTML TEXT 
 no_of_as = len(list_as)
 
 print("no of img tags", no_of_imgs)
